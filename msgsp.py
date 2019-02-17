84b8a0d2bf034907882533b3388afeae1ff4c24f
Kushagradhi Bhowmik <rivu.ilsssd@gmail.com>
2019-02-17 15:46:22 -0600

    docstrings added

--- msgsp.py
+++ msgsp.py
@@ -4,24 +4,24 @@ import copy
 
 class MSGsp:
 	def __init__(self,S,MS,n,SDC):
-		self.S     = S    							# Data Sequences
-		self.MS    = MS								# Minimum Suupport of items
-		self.n     = n								# Total number of unique items
-		self.SDC   = SDC							# User defined Support Difference Constraint
-		self.M     = None							# Items sorted according to Min. Item Support stored in MS
-		self.L     = []								# seeds set for generating the set of candidate sequences of length 2
-		self.SC    = {}								# Actual support count of items from given data
-		self.Count = {}
-		self.F     = OrderedDict()					# Frequent k-Sequences
-		self.C     = OrderedDict()					# Candidate k-Sequences
-		self.di    = {}							# Dictionary for candidate sequence duplicate check
-		self.filename = "results.txt"				# Output File
+		self.S        = S		# Data Sequences
+		self.MS       = MS		# Minimum Suupport of items
+		self.n        = n		# Total number of unique items
+		self.SDC      = SDC		# User defined Support Difference Constraint
+		self.M        = None	# Items sorted according to Min. Item Support stored in MS
+		self.L        = []		# seeds set for generating the set of candidate sequences of length 2
+		self.SC       = {}		# Actual support count of items from given data
+		self.di       = {}		# Dictionary for candidate sequence duplicate check
+		self.Count    = {}
+		self.F        = OrderedDict()	# Frequent k-Sequences
+		self.C        = OrderedDict()	# Candidate k-Sequences
+		self.filename = "results.txt"	# Output File
 
-		self._msgsp()								# Calls the MS-GSP algorithm
-		self._outputResult()						# Writes data to output file
+		self._msgsp()	# Calls the MS-GSP algorithm
+		self._outputResult()	# Writes data to output file
 
 	def _msgsp(self):
-		k = 2										# K initialized to 2-length sequence	
+		k = 2	# k initialized to 2-length sequence	
 		self._sort()								
 		self._initPass()
 		self._f1()
@@ -49,10 +49,15 @@ class MSGsp:
 			k+=1
 
 	def _sort(self):
+		"""Sort items according to Min. Support"""
 		print ("Sort")
 		self.M = OrderedDict(sorted(self.MS.items(),key=lambda t:t[1]))
 
 	def _initPass(self):
+		"""First pass over S
+		
+		   Produces seed set L for generating C2 and recods support count of items.
+		"""
 		print ("Init Pass")
 		lMISItem  = None	#? unused
 		lMIS      = None
@@ -70,6 +75,7 @@ class MSGsp:
 				self.L.append(item)
 
 	def _f1(self):
+		"""Generates Frequent 1-sequences (F1)"""
 		F1 = {}
 		for item in self.L:
 			if self.SC[item] >= self.MS[item]:
@@ -79,6 +85,7 @@ class MSGsp:
 		self.F[1] = F1
 
 	def _level2CandidateGenSPM(self):
+		"""Generates candidate 2-sequences (C2)"""
 		print ("Level2 Candidate Generation")
 		C2 = []
 		for idx, l in enumerate(self.L):
@@ -98,6 +105,7 @@ class MSGsp:
 		self.C[2] = C2
 	
 	def _MSCandidateGenSPM(self,k):
+		"""Generates candidate k-sequences (k>2)"""
 		print ("MS Candidate Generation for : {}".format(k))
 		F = self.F[k-1]
 		self.C[k] = []
@@ -114,6 +122,14 @@ class MSGsp:
 		self._prune(k)
 
 	def _ForwardCandidateGenSPM(self,seq1,seq2,isFirst,k):
+		"""Generates candidate sequence for conditions where MIS of first item of first sequence or MIS of last sequence of S2 is less than MIS of other items in candidate generation loop
+		
+		Args:
+			seq1: first sequence
+			seq2: second sequence
+			isFirst: True/False indicates whether first or second case in above description
+			k: k in k-sequence
+		"""
 		s1 = seq1.sequence if isFirst else [list(reversed(itemset)) for itemset in list(reversed(seq2.sequence))]
 		s2 = seq2.sequence if isFirst else [list(reversed(itemset)) for itemset in list(reversed(seq1.sequence))]
 		nseq1, nseq2 = None, None
@@ -154,6 +170,13 @@ class MSGsp:
 				self.C[k].append(nseq2)
 
 	def _lowestMIS(self,s,isFirst,k):
+		"""Generates lowest MIS Item
+		
+		Args:
+			s: sequence to find lowest MIS
+			isFirst: True/False determines whether to check first or last item for lowest MIS
+			k: k in k-sequence
+		"""
 		x        = 0 if isFirst else -1
 		item     = s[x][x]
 		itemMIS  = self.MS[item]
@@ -167,10 +190,25 @@ class MSGsp:
 		return True
 
 	def _newSequence(self,k,s,minMISItem,isFirst):
+		"""Generates candidate sequence for conditions where MIS of first item of first sequence or MIS of last sequence of S2 is less than MIS of other items  in candidate generation loop
+		
+		Args:
+			k: k in k-sequence
+			s: sequence
+			minMISItem: item with lowest MIS
+			isFirst: True/False indicates whether first or second case in above description
+		"""		
 		sequence = Sequence(s,minMISItem) if isFirst else Sequence(reversedSequence(s),minMISItem)
 		return sequence
 
 	def _candidateGenSPM(self,seq1,seq2,k):
+		"""Generates candidate sequence for else condition in candidate generation loop
+		
+		Args:
+			seq1: first sequence
+			seq2: second sequence
+			k: k in k-sequence
+		"""
 		s1, s2   = seq1.sequence, seq2.sequence
 		_s1, _s2 = copy.deepcopy(s1), copy.deepcopy(s2)
 		item     = _s2[-1][-1]
@@ -199,6 +237,11 @@ class MSGsp:
 				self.C[k].append(nseq1)
 
 	def _frequentSequence(self,k):
+		"""Generates set Fk of frequent k-sequences from Ck
+		
+		Args:
+			k: k in k-sequence
+		"""
 		F = {}
 		for c in self.C[k]:
 			if c.count/self.n >= self.MS[c.minMISItem]:
@@ -207,6 +250,11 @@ class MSGsp:
 		self.F[k] = F
 
 	def _prune(self,k):
+		"""Prunes k sequences from Ck
+		
+		Args:
+			k: k in k-sequence
+		"""
 		toRemove = []
 		for idx, c in enumerate(self.C[k]):
 			if not self._allFrequentk_1(c,k):
@@ -216,6 +264,12 @@ class MSGsp:
 		 	self.C[k].remove(c)
 	
 	def _allFrequentk_1(self,c,k):
+		"""Returns True/False conditioned on whether k-1 sequences (except subsequence formed by removing item with strictly lowest MIS value) are frequent
+		
+		Args:
+			c: candidate sequence for which to extract k-1 frequent sequences
+			k: k in k-sequence
+		"""
 		for i, itemset in enumerate(c.sequence):
 			for j, item in enumerate(itemset):
 				if item == c.minMISItem and c.minMISItemCount == 1:
@@ -229,6 +283,7 @@ class MSGsp:
 		return True
 
 	def _outputResult(self):
+		"""Write frequent k-sequences generated into output file"""
 		fp = open(self.filename,"w")
 		for k,fk in self.F.items():
 			if (len(fk)>0):
